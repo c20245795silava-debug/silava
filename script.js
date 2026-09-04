b6401062db6b289ef27523b830848a2ac608b813
mycca5305 <c20245795.silava@csav.edu.ph>
2026-09-04 17:57:30 +0800

    First Javascript

--- script.js
+++ script.js
@@ -3,49 +3,48 @@ console.log("From script.js file.");
 
 const myName = "mycca";
 let age = 21;
-age = 20;
 let number = 995440209;
-const address = "Miraflores";
+let address = "Miraflores";
 
-console.log(`My name is ${myName}`);
+console.log(`Name: ${myName}`);
 console.log(`Age: ${age}`);
 console.log(`Number: ${number}`);
 console.log(`Address: ${address}`);
 console.log(67);
 
 
-function greet(name) {
-    if (Array.isArray(name)) {
-        name = name.join(", ");
+function greet(greetings, names = []) {
+    if (Array.isArray(names)) {
+        names = names.join(", ");
     }
-    return `${greetings} sa enyu ${name}`;
+    return `${greetings} sa enyu ${names}`;
 }
 
 console.log(greet("Ma ayong aga ", ["mycca", "Bai", "Soy", "Erp"]));
 
 const heading = document.querySelector("h1");
-heading.textContent = "Cesar";
+heading.textContent = "Cesar Silava ";
 heading.style.color = "purple";
 heading.style.backgroundColor = "yellow";
 heading.style.fontSize = "100px";
-console.log(heading.textContent);
+console.log(heading);
 
 heading.addEventListener("click", function () {
     heading.style.color = "red";
     console.log("Heading was clicked.");
-})
+});
 
 const contactHeading = document.querySelector("#contact h2");
 console.log(contactHeading);
 
-const projectsHeading = document.querySelector("#projects h2");
+const projectsHeading = document.querySelector("#services h2");
 console.log(projectsHeading);
 
 const togglebutton = document.querySelector("#switch");
 const body = document.querySelector("body");
 let issOff = false;
 
-toggleButton.addEventListener("click", function () {
+togglebutton.addEventListener("click", function () {
     isOff = !isOff;
 
     if (isOff) {
