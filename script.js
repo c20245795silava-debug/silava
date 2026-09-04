78ea0935439ceed7b24ce4d011280e740cf40334
vinsonandrea <c20245391.vinson@csav.edu.ph>
2026-09-04 14:44:24 +0800

    First Javascript

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ console.log("From script.js file.");
 
 const myName = "mycca";
 let age = 21;
+let number = 995440209;
+const address = "Miraflores";
 
-console.log('My name is ${myName}, ${age} years old.');
+console.log(`My name is ${myName}, ${age} years old.`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
+console.log(67);
+
